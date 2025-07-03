86548262d76f73464324ecf0e11b4f700aad5dcb
dieg0mtz <diegomtzram803@gmail.com>
2025-07-03 15:54:24 -0600

    Cambio de imagen para examen Parcial 2 Diego Martinez

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import imagenEstudiante from './assets/imagenEstudiante.png'
+import imagenEstudiante from './assets/imagenEstudiante.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
